6f8c84468af6924e08eb2b17e02f48c1f6d0ffb0
Alisue <lambdalisue@gmail.com>
2024-08-13 01:02:48 +0900

    feat(dropWhile): refine `fn` signature and docs

--- drop_while.ts
+++ drop_while.ts
@@ -4,6 +4,10 @@
  * The first element that does not match the predicate is included in the output.
  * If the predicate never returns false, the output will be an empty iterable.
  *
+ * Use {@linkcode https://jsr.io/@core/iterutil/drop drop} to drop a specific number of elements.
+ * Use {@linkcode https://jsr.io/@core/iterutil/take take} to take a specific number of elements.
+ * Use {@linkcode https://jsr.io/@core/iterutil/async/drop-while dropWhile} to drop elements asynchronously.
+ *
  * @param iterable The iterable to drop elements from.
  * @param fn The predicate function to drop elements with.
  * @returns The iterable with elements dropped while the predicate returns true.
@@ -12,17 +16,21 @@
  * ```ts
  * import { dropWhile } from "@core/iterutil/drop-while";
  *
- * const iter = dropWhile([1, 2, 3, 4, 5], (x) => x < 3);
+ * const iter = dropWhile(
+ *   [1, 2, 3, 4, 5],
+ *   (v) => v < 3,
+ * );
  * console.log(Array.from(iter)); // [3, 4, 5]
  * ```
  */
 export function* dropWhile<T>(
   iterable: Iterable<T>,
-  fn: (value: T) => boolean,
+  fn: (value: T, index: number) => boolean,
 ): Iterable<T> {
   let dropping = true;
+  let index = 0;
   for (const value of iterable) {
-    if (dropping && fn(value)) {
+    if (dropping && fn(value, index++)) {
       continue;
     }
     dropping = false;
