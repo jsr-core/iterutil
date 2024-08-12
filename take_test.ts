53b963e488f1bcd0dca262e38bb0498e6daf4b65
Alisue <lambdalisue@gmail.com>
2024-08-13 01:17:41 +0900

    feat!(take): refine impl, `limit` guard, and docs

--- take_test.ts
+++ take_test.ts
@@ -1,6 +1,7 @@
 import { assertEquals, assertThrows } from "@std/assert";
 import { assertType, type IsExact } from "@std/testing/types";
-import { take, TakeLimitError } from "./take.ts";
+import { iter } from "./iter.ts";
+import { take } from "./take.ts";
 
 Deno.test("take", async (t) => {
   await t.step("with positive limit", () => {
@@ -10,19 +11,30 @@ Deno.test("take", async (t) => {
     assertType<IsExact<typeof result, Iterable<number>>>(true);
   });
 
-  await t.step("with negative limit", () => {
-    assertThrows(
-      () => {
-        take([0, 1, 2, 3, 4], -2);
-      },
-      TakeLimitError,
-    );
-  });
-
   await t.step("with 0 limit", () => {
     const result = take([0, 1, 2, 3, 4], 0);
     const expected: number[] = [];
     assertEquals(Array.from(result), expected);
     assertType<IsExact<typeof result, Iterable<number>>>(true);
+  });
+
+  await t.step("will stop consuming once limit items is taken", () => {
+    const it = iter([0, 1, 2, 3, 4]);
+    const result = take(it, 3);
+    const expected: number[] = [0, 1, 2];
+    assertEquals(Array.from(result), expected);
+    assertType<IsExact<typeof result, Iterable<number>>>(true);
+    // Ensure the iterator is NOT fully consumed
+    assertEquals(Array.from(it), [3, 4]);
+  });
+
+  await t.step("throws RangeError", async (t) => {
+    await t.step("if the limit is not 0 nor positive safe integer", () => {
+      assertThrows(() => take([], NaN), RangeError);
+      assertThrows(() => take([], Infinity), RangeError);
+      assertThrows(() => take([], -Infinity), RangeError);
+      assertThrows(() => take([], -1), RangeError);
+      assertThrows(() => take([], 1.1), RangeError);
+    });
   });
 });
