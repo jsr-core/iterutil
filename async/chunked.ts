bfcea9fdc5606e6e27a5a07f51baba7373498f07
Alisue <lambdalisue@gmail.com>
2024-08-13 00:51:38 +0900

    feat(chunked): refine `size` guard and docs

--- async/chunked.ts
+++ async/chunked.ts
@@ -1,31 +1,47 @@
 /**
- * Chunks an iterable into arrays of a given size.
+ * Chunks the iterable into the iterable of arrays of `size`.
+ *
+ * The last chunk may have less than `size` elements.
+ *
+ * Use {@linkcode https://jsr.io/@core/iterutil/async/flatten flatten} to flatten the chunks.
+ * Use {@linkcode https://jsr.io/@core/iterutil/chunked chunked} to chunk iterables synchronously.
  *
  * @param iterable The iterable to chunk.
  * @param size The size of each chunk.
  * @returns The chunked iterable.
+ * @throws {RangeError} if `size` is not a positive safe integer.
  *
  * @example
  * ```ts
  * import { chunked } from "@core/iterutil/async/chunked";
  *
- * const iter = chunked([1, 2, 3, 4, 5], 2);
+ * const iter = chunked(
+ *   [1, 2, 3, 4, 5],
+ *   2,
+ * );
  * console.log(await Array.fromAsync(iter)); // [[1, 2], [3, 4], [5]]
  * ```
  */
-export async function* chunked<T>(
+export function chunked<T>(
   iterable: Iterable<T> | AsyncIterable<T>,
   size: number,
 ): AsyncIterable<T[]> {
-  let chunk = [];
-  for await (const item of iterable) {
-    chunk.push(item);
-    if (chunk.length === size) {
-      yield chunk;
-      chunk = [];
+  if (size <= 0 || !Number.isSafeInteger(size)) {
+    throw new RangeError(
+      `size must be a positive safe integer, but got ${size}.`,
+    );
+  }
+  return async function* () {
+    let chunk = [];
+    for await (const item of iterable) {
+      chunk.push(item);
+      if (chunk.length === size) {
+        yield chunk;
+        chunk = [];
+      }
     }
-  }
-  if (chunk.length > 0) {
-    yield chunk;
-  }
+    if (chunk.length > 0) {
+      yield chunk;
+    }
+  }();
 }
